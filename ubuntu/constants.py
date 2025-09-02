f443b4d6c9086055e9309e23b3ef4f4684a2b9ab
Niranjan Vemana <nvemana@qti.qualcomm.com>
2025-09-01 20:42:04 -0700

    Extracting vmlinux to out directory
    vmlinux is extracted from qcom-tool deb
    version of qcom-tools is similar to other kernel deb
    vmlinux is then populated to out folder
    merge manifest is consumed from helpers
    
    Change-Id: I17873295a79370e556edf48fd3d132d319e68b1f

--- ubuntu/constants.py
+++ ubuntu/constants.py
@@ -1,5 +1,6 @@
 import os
 
+LINUX_IMAGE_DBGSYM_DEB = "oss/linux-qcom-tools*/linux-qcom-tools*_arm64.deb"
 LINUX_MODULES_DEB = "linux-modules-*-qcom/linux-modules-*_arm64.deb"
 
 KERNEL_DEBS = [
@@ -16,6 +17,7 @@ KERNEL_DEBS = [
 ]
 
 COMBINED_DTB_FILE  = "combined-dtb.dtb"
+VMLINUX_QCOM_FILE  = "vmlinux"
 IMAGE_NAME         = "system.img"
 
 IMAGE_SIZE_IN_G     = 8
