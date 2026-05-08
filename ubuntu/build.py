d161f6a156700cda2c0814212f22b5015c415695
Kasin Li <donglil@qti.qualcomm.com>
2026-05-08 16:29:30 +0800

    add framewors to build path
    We need camera_metadata under it.
    
    Change-Id: Ic12ce9b6c72f5721dd20bc0f8d15e88ef0e7158a

--- ubuntu/build.py
+++ ubuntu/build.py
@@ -214,6 +214,7 @@ SOURCES_DIRS = [
     # Add more directories to scan for debian packages here, e.g.:
     # os.path.join(WORKSPACE_DIR, "vendor"),
     os.path.join(WORKSPACE_DIR, "sources", "quic-qrb-ros", "ocr_service"),
+    os.path.join(WORKSPACE_DIR, "frameworks"),
     os.path.join(WORKSPACE_DIR, "graphics"),
     os.path.join(WORKSPACE_DIR, "system"),
     os.path.join(WORKSPACE_DIR, "vendor", "qcom"),
