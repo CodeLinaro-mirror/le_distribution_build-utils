466d1c154ebdb0055652033d879a751d00f08f70
Lei wang <leiwan@qti.qualcomm.com>
2026-06-03 15:58:30 +0800

    build-utils: support auto grow rootfs
    Change-Id: I1734219144665b96ad90823cd3e35b6415cc60df
    Signed-off-by: Lei wang <leiwan@qti.qualcomm.com>

--- ubuntu/pack_deb.py
+++ ubuntu/pack_deb.py
@@ -301,7 +301,7 @@ mmdebstrap --verbose --variant=apt --logfile={log_file} \
 --customize-hook='find "$1" \\( -path "$1/home" -o -path "$1/root" -o -path "$1/tmp" -o -path "$1/run" -o -path "$1/proc" -o -path "$1/sys" -o -path "$1/dev" \\) -prune -o -type d -not -perm -o+x -print0 | xargs -0 -r chmod o+rx' \
 --customize-hook='tar -C "$1" --exclude=./proc --exclude=./sys --exclude=./dev --exclude=./run -cf {rootfs_tar} .' \
 --setup-hook='rm -rf "$1/var/lib/apt/lists" "$1/var/cache/apt" "$1/var/cache/man" "$1/var/lib/landscape" "$1/var/log/landscape" "$1/home" 2>/dev/null; mkdir -p "$1/var/lib/apt/lists/partial" "$1/var/cache/apt" "$1/home"; true' \
---setup-hook='echo /dev/disk/by-partlabel/system / ext4 defaults 0 1 > "$1/etc/fstab"' \
+--setup-hook='echo /dev/disk/by-partlabel/system / ext4 defaults,x-systemd.growfs 0 1 > "$1/etc/fstab"' \
 --setup-hook='echo PARTLABEL=modem /firmware vfat defaults,ro >> "$1/etc/fstab"' \
 """
 
